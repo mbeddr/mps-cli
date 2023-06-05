14126856c7e11c168e9537446f415e150678e96b
Konstantin Kolmar <konstantin.kolmar@lunatech.nl>
2023-06-05 23:31:19 +0300

    mps_cli.gradle-plugin: only use files modified in the current branch (#3)
    * mps_cli.gradle-plugin: only use files modified in the current branch when computing cone of influence
    
    * bumped version to 0.6

--- mps-cli-gradle-plugin/plugin/src/main/groovy/org/mps_cli/cone_of_influence/GitFacade.groovy
+++ mps-cli-gradle-plugin/plugin/src/main/groovy/org/mps_cli/cone_of_influence/GitFacade.groovy
@@ -2,9 +2,9 @@ package org.mps_cli.cone_of_influence
 
 class GitFacade {
 
-    static List<String> computeFilesWhichAreDifferentFromBranch(gitRepoLocation, branchName) {
+    static List<String> computeFilesWhichAreModifiedInCurrentBranch(gitRepoLocation, branchName) {
         def sout = new StringBuilder(), serr = new StringBuilder()
-        def proc = "git diff --name-only $branchName".execute([], new File(gitRepoLocation))
+        def proc = "git diff --name-only --merge-base $branchName".execute([], new File(gitRepoLocation))
         proc.consumeProcessOutput(sout, serr)
         proc.waitForOrKill(5000)
 
